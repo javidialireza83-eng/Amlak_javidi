f42335d23cac34999aee38260a5cf32944c0cc6b
javidialireza83-eng <javidialireza83@gmail.com>
2026-07-06 23:06:52 +0330

    Fix STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 # MIDDLEWARE = [
